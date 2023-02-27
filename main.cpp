1f85fdf65923f15fab6754d41119e24e794e0eea
Rodrigo Aranda <raranda@ucatolica.edu.co>
2023-02-27 15:12:44 -0500

    proyecto base del ejercicio

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> oldPassword;
+bool saveLastPassword(string newPassword);
+void initializerPaswwords();
+
+int main()
+{
+    for(auto i : oldPassword){
+        cout<<i<<endl;
+    }
+
+    bool salida = saveLastPassword("clave7");
+    if(salida){
+        cout<<"se adicionó la clave";
+    }else
+        cout<<"contraseña ya ha sido usada";
+
+    cout<<endl;
+    for(auto i : oldPassword){
+       cout<<i<<endl;
+        }
+
+       return 0;
+}
+
+bool saveLastPassword(string newPassword){
+    bool isUsed= false;
+    for(auto x : oldPassword){
+        if(x.compare(newPassword)==0){
+                isUsed = true;
+                break;
+        }
+    }
+    if(!isUsed){
+        reverse(oldPassword.begin(),oldPassword.end());
+        oldPassword.pop_back();
+        reverse(oldPassword.begin(),oldPassword.end());
+        oldPassword.push_back(newPassword);
+    }
+    return isUsed;
+}
+
+void initializerPaswwords(){
+    oldPassword.push_back("clave1");
+    oldPassword.push_back("clave2");
+    oldPassword.push_back("clave3");
+    oldPassword.push_back("clave4");
+    oldPassword.push_back("clave5");
+}
